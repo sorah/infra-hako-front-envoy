c2f643a1120d7af1adabbbaa2ee8122e8fb345e5
Sorah Fukumori <sorah@cookpad.com>
2020-08-31 13:50:36 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM envoyproxy/envoy:v1.15-latest
 
-COPY run.sh /run.sh
-COPY config.yml /config.yml
-
 # for fault injection
 RUN mkdir -p /srv/runtime/current/envoy
 RUN mkdir -p /srv/runtime/current/envoy_override
+RUN mkdir -p /work
+RUN chown envoy /work
+
+COPY run.sh /run.sh
+COPY config.yml /config.yml
 
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ENVOY_OPTS="-c /config.gen.yml"
+ENVOY_OPTS="-c /work/config.yaml"
 
 if [[ -n "${ENVOY_CONCURRENCY}" ]]; then
   ENVOY_OPTS="${ENVOY_OPTS} --concurrency ${ENVOY_CONCURRENCY}"
 fi
-cp /config.yml /config.gen.yml
-sed -i -e "s/||ADDRESS||/${ADDRESS:-app}/g" /config.gen.yml
-sed -i -e "s/||PORT||/${PORT:-8080}/g" /config.gen.yml
+cp /config.yml /work/config.yaml
+sed -i -e "s/||ADDRESS||/${ADDRESS:-app}/g" /work/config.yaml
+sed -i -e "s/||PORT||/${PORT:-8080}/g" /work/config.yaml
 exec envoy $ENVOY_OPTS

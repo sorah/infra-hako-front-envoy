63409cc5edecb8dde222c7591aefdf9bd664bde1
Sorah Fukumori <sorah@cookpad.com>
2020-08-31 13:27:12 +0900

    fix config

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-ENVOY_OPTS="-c /config.yml"
+ENVOY_OPTS="-c /config.gen.yml"
 
 if [[ -n "${ENVOY_CONCURRENCY}" ]]; then
   ENVOY_OPTS="${ENVOY_OPTS} --concurrency ${ENVOY_CONCURRENCY}"
 fi
-sed -i -e "s/||ADDRESS||/${ADDRESS:-app}/g" /config.yml
-sed -i -e "s/||PORT||/${PORT:-8080}/g" /config.yml
+cp /config.yml /config.gen.yml
+sed -i -e "s/||ADDRESS||/${ADDRESS:-app}/g" /config.gen.yml
+sed -i -e "s/||PORT||/${PORT:-8080}/g" /config.gen.yml
 exec envoy $ENVOY_OPTS

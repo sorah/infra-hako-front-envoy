a11af1f8d4bab5a4f4be09c2cc177c160cdec44d
Sorah Fukumori <sorah@cookpad.com>
2020-08-31 14:18:38 +0900

    oops

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ cp /config.yml /work/config.yaml
 sed -i -e "s/||ADDRESS||/${ADDRESS:-app}/g" /work/config.yaml
 sed -i -e "s/||PORT||/${PORT:-8080}/g" /work/config.yaml
 if [[ -n "${HTTP2}" ]]; then
-  sed -i -e "s/||HTTP2||/http2_protocol_options: {}/g" /work/config.yml
+  sed -i -e "s/||HTTP2||/http2_protocol_options: {}/g" /work/config.yaml
 else
-  sed -i -e "s/||HTTP2||//g" /work/config.yml
+  sed -i -e "s/||HTTP2||//g" /work/config.yaml
 fi
 exec envoy $ENVOY_OPTS
